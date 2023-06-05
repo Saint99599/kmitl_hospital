90e8e254f136ba6c55bc2f20421acb9778e76b1e
zergreen <kawhao101@gmail.com>
2023-06-05 21:22:22 +0700

    implement [green] api

---
+++ src/function/AuthApi.js
@@ -0,0 +1,130 @@
+import axios from 'axios';
+
+const login = async (username, password, navigate) => {
+    
+    const url = "http://localhost:3000/auth/login";
+    await axios({
+      method: "POST",
+      url: url,
+      data: {
+        username: username,
+        password: password
+      },
+    })
+      .then((response) => {
+        console.log(response.data.accessToken);
+        localStorage.setItem("TOKEN", response.data.accessToken);
+        navigate("/home");
+        // window.location.replace('/home');
+      })
+      .catch((err) => {
+          console.log('34');
+          console.log(err);
+        alert('Failed')
+      });
+  }
+
+  const register = async (username, password, role, navigate) => {
+    const qs = require("qs");
+    let data = qs.stringify({
+      Employee_ID: "1",
+      Myuser_Username: username,
+      Myuser_Password: password,
+      Myuser_Role: role,
+    });
+
+    let config = {
+      method: "POST",
+      maxBodyLength: Infinity,
+      url: "http://localhost:3000/auth/register",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      data: data,
+    };
+
+    axios
+      .request(config)
+      .then((response) => {
+        console.log(JSON.stringify(response.data));
+        navigate('/')
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const getProfile = () => {
+    // alert('hello')
+    const token = localStorage.getItem('TOKEN')
+    // alert(token);
+    axios({
+      method: 'GET',
+      maxBodyLength: Infinity,
+      url: 'http://localhost:3000/auth/protected',
+      headers: { 
+        'Authorization': `Bearer ${token}`
+      }
+    }).then((response) => {
+      console.log(response.data);
+      alert(JSON.stringify(response.data) + `\nTOKEN: ${token}`)
+      // alert('TOKEN: '+token)
+    }).catch((error) => {
+      console.log(error);
+      alert('can not access becase token is expired')
+    })
+    console.log('1');
+  }
+
+  const getLogout = () => {
+    const token = localStorage.getItem('TOKEN');
+    axios({
+      method: 'POST',
+      maxBodyLength: Infinity,
+      url: 'http://localhost:3000/auth/logout',
+      headers: { 
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      data : {
+        refreshToken: token
+      }
+    }).then((response) => {
+      console.log(JSON.stringify(response.data));
+      localStorage.clear();
+    //   navigate("/",{ replace: true });
+      // window.location('/login');
+      // alert(response.data)
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
+
+  const getRefreshToken = (time) => {
+    setInterval(() => {
+        const token = localStorage.getItem('TOKEN');
+        console.log(`${token}`);
+        axios({
+          method: "POST",
+          maxBodyLength: Infinity,
+          url: "http://localhost:3000/auth/refresh-token",
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          data: {
+            refreshToken: `${token}`,
+          },
+        })
+          .then((response) => {
+            console.log(response.data);
+            localStorage.setItem("TOKEN", response.data.accessToken);
+          })
+          .catch((error) => {
+            localStorage.clear()
+            alert('cannot refresh token : you must Re Login Again');
+            window.location.replace('/');
+            console.log(error);
+          });
+      }, time);
+  }
+
+export { login, register, getProfile, getLogout, getRefreshToken };
--- src/page/Login.js
+++ src/page/Login.js
@@ -1,19 +1,21 @@
 import React, { useState } from "react";
 import { useNavigate } from 'react-router-dom';
+import { login } from '../function/AuthApi';
 
 const Login = () => {
   const [username, setUser] = useState("");
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
-
+  
   const handleLogin = (e) => {
+    const x = login(username, password, navigate);
     e.preventDefault();
 
-    if (username === "qwerty" && password === "qwerty") {
-        navigate("/home");
-    } else {
-        alert("Login fail");
-    }
+     // if (username === "qwerty" && password === "qwerty") {
+    //     navigate("/home");
+    // } else {
+    //     alert("Login fail");
+    // }
   };
 
   return (
@@ -22,6 +24,7 @@ const Login = () => {
             <div className="w-2/3 h-2/3 p-4 ">
                 <h2 className="text-3xl my-2.5">ยินดีต้อนรับ</h2>
                 <h1 className="text-5xl leading-6 my-2.5 pb-4">เข้าสู่ระบบ</h1>
+                
                 <form onSubmit={handleLogin}>
                     <div className="my-5">
                         <p className="font-roboto font-normal font-400 text-base leading-5 text-gray-700 pb-2">Username</p>
@@ -30,7 +33,7 @@ const Login = () => {
                             type="text"
                             value={username}
                             onChange={(e) => setUser(e.target.value)}
-                            required
+                            // required
                         />
                     </div>
                     <div className="my-5">
@@ -40,11 +43,12 @@ const Login = () => {
                             type="password"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
-                            required
+                            // required
                         />
                     </div>
                     <button className="bg-orange-500 rounded-lg font-roboto font-bold font-700 text-lg leading-6 text-white w-full h-12" type="submit">เข้าสู่ระบบ</button>
                 </form>
+            <a href="/register">คุณยังไม่มีบัญชีใช่หรือไม่? [click]</a>
             </div>
         </div>
     </div>

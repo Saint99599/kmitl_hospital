48f9108a56ab3ccd478f7c850cbdb273195f1383
zergreen <kawhao101@gmail.com>
2023-06-05 23:23:43 +0700

    add: register route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { BookingProvider } from './function/BookingContext';
 
 import Login from './page/Login';
+import Register from './page/Register';
 import Home from './page/Home';
 import Booking from './page/ฺBooking';
 import InputBooking from './page/InputBooking';
@@ -18,6 +19,7 @@ function App() {
           <Route exact path="/booking" element={<Booking />} />
           <Route exact path="/inputbooking" element={<InputBooking/>} />
           <Route exact path="/confirmbooking" element={<ConfirmBooking/>} />
+          <Route exact path="/register" element={<Register/>} />
         </Routes>
       </BookingProvider>
     </BrowserRouter>
